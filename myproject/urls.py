c479376ee8834dc69bee6f1c01a4e02512c172ea
onana <onanadonald@gmail.com>
2022-01-12 23:50:46 +0100

    error

--- myproject/urls.py
+++ myproject/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
     path('api/', include('rest_api.urls')),
 
 
-   path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
